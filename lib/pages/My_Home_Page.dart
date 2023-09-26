36f8e151abb33f3e49109f72e1f10fdafeed2952
Jake Cabreros <rjake909@gmail.com>
2023-09-26 22:06:17 +0800

    Add ScrollView

--- lib/pages/My_Home_Page.dart
+++ lib/pages/My_Home_Page.dart
@@ -8,61 +8,80 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Online Master Class'),
-          backgroundColor: Theme.of(context).primaryColor,
-        ),
-        body: Column(
+      appBar: AppBar(
+        title: Text('Online Master Class'),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
           children: [
-            Row(
-              children: [
-                CrtCard(
-                  cardtitle: 'SP Elec 1B',
-                  cardsubtitle: 'Mobile Application',
-                  width: 200,
-                  height: 300,
-                ),
-                CrtCard(
-                  cardtitle: 'SP Elec 1B',
-                  cardsubtitle: 'Mobile Application',
-                  width: 200,
-                  height: 300,
-                ),
-                CrtCard(
-                  cardtitle: 'SP Elec 1B',
-                  cardsubtitle: 'Mobile Application',
-                  width: 200,
-                  height: 300,
-                ),
-              ],
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  CrtCard(
+                    cardtitle: 'SP Elec 1B',
+                    cardsubtitle: 'Mobile Applications',
+                    width: 200,
+                    height: 300,
+                  ),
+                  CrtCard(
+                    cardtitle: 'AppsDev',
+                    cardsubtitle: 'HTML CSS JS',
+                    width: 200,
+                    height: 300,
+                  ),
+                  CrtCard(
+                    cardtitle: 'IM2',
+                    cardsubtitle: 'Data Management',
+                    width: 200,
+                    height: 300,
+                  ),
+                  CrtCard(
+                    cardtitle: 'Net2',
+                    cardsubtitle: 'Networking',
+                    width: 200,
+                    height: 300,
+                  ),
+                ],
+              ),
             ),
             ListTile(
               title: Text('Free Online Class'),
-              subtitle: Text('Form Over 80 Lectures'),
+              subtitle: Text('For over 80 lectures'),
             ),
             Column(
               children: [
                 CrtCard(
-                  cardtitle: 'CodeLab',
-                  cardsubtitle: 'Make your first App',
-                  width: 450,
-                  height: 150,
+                  cardtitle: 'OOP',
+                  cardsubtitle: 'Object Oreinted Programing',
+                  width: 330,
+                  height: 120,
+                ),
+                CrtCard(
+                  cardtitle: 'Programing',
+                  cardsubtitle: 'Learn in C',
+                  width: 330,
+                  height: 120,
                 ),
                 CrtCard(
                   cardtitle: 'CodeLab',
                   cardsubtitle: 'Make your first App',
-                  width: 450,
-                  height: 150,
+                  width: 330,
+                  height: 120,
                 ),
                 CrtCard(
-                  cardtitle: 'CodeLab',
-                  cardsubtitle: 'Make your first App',
-                  width: 450,
-                  height: 150,
+                  cardtitle: 'DVA',
+                  cardsubtitle: 'Digital Arts',
+                  width: 330,
+                  height: 120,
                 ),
               ],
             ),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
