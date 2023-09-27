d7ee6d08f3cb4414afa2de181e9668753b45fa9d
Jake Cabreros <rjake909@gmail.com>
2023-09-27 17:34:55 +0800

    Instanciate cards and add to main_page

--- lib/content/cards.dart
+++ lib/content/cards.dart
@@ -55,3 +55,76 @@ class CrtCard extends StatelessWidget {
     );
   }
 }
+
+var card0 = CrtCard(
+  cardtitle: 'SP Elec 1B',
+  cardsubtitle: 'Mobile Applications',
+  cardimage: 'image/Saly-card1.png',
+  scale: 1.5,
+  width: 200,
+  height: 300,
+  id: 0,
+);
+var card1 = CrtCard(
+  cardtitle: 'AppsDev',
+  cardsubtitle: 'HTML CSS JS',
+  cardimage: 'image/Saly-card2.png',
+  scale: 1.5,
+  width: 200,
+  height: 300,
+  id: 1,
+);
+var card2 = CrtCard(
+  cardtitle: 'IM2',
+  cardsubtitle: 'Data Management',
+  cardimage: 'image/Saly-card1.png',
+  scale: 1.5,
+  width: 200,
+  height: 300,
+  id: 2,
+);
+var card3 = CrtCard(
+  cardtitle: 'Net2',
+  cardsubtitle: 'Networking',
+  cardimage: 'image/Saly-card2.png',
+  scale: 1.5,
+  width: 200,
+  height: 300,
+  id: 3,
+);
+var card4 = CrtCard(
+  cardtitle: 'OOP',
+  cardsubtitle: 'Object Oreinted Programing',
+  cardimage: 'image/Saly-20.png',
+  scale: 1,
+  width: 330,
+  height: 120,
+  id: 4,
+);
+var card5 = CrtCard(
+  cardtitle: 'Programing',
+  cardsubtitle: 'Learn in C',
+  cardimage: 'image/Saly-21.png',
+  scale: 1,
+  width: 330,
+  height: 120,
+  id: 5,
+);
+var card6 = CrtCard(
+  cardtitle: 'CodeLab',
+  cardsubtitle: 'Make your first App',
+  cardimage: 'image/Saly-23.png',
+  scale: 1,
+  width: 330,
+  height: 120,
+  id: 6,
+);
+var card7 = CrtCard(
+  cardtitle: 'DVA',
+  cardsubtitle: 'Digital Arts',
+  cardimage: 'image/Saly-24.png',
+  scale: 1,
+  width: 330,
+  height: 120,
+  id: 7,
+);
